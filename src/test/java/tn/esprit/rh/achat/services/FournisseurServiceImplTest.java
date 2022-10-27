233a99544f1754d265f677d2c9b512c143014e6e
Hibouch <47923944+Hibouch@users.noreply.github.com>
2022-10-27 18:38:03 +0100

    27/10/2022

--- src/test/java/tn/esprit/rh/achat/services/FournisseurServiceImplTest.java
+++ src/test/java/tn/esprit/rh/achat/services/FournisseurServiceImplTest.java
@@ -25,7 +25,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 @RunWith(SpringRunner.class)
 @ExtendWith(MockitoExtension.class)
-//@SpringBootTest
+@SpringBootTest
 @Slf4j
 public class FournisseurServiceImplTest {
 	
