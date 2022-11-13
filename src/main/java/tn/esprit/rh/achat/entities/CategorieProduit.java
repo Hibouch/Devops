4a2b64ea9115c75dd6ccce6b9cbc6165df25f146
iheb-marweni <79664116+iheb-marweni@users.noreply.github.com>
2022-11-13 19:07:15 +0100

    s

--- src/main/java/tn/esprit/rh/achat/entities/CategorieProduit.java
+++ src/main/java/tn/esprit/rh/achat/entities/CategorieProduit.java
@@ -27,7 +27,7 @@ public class CategorieProduit implements Serializable {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idCategorieProduit;
-	int iheb ;
+	int ieb ;
 	private String codeCategorie;
 	private String libelleCategorie;
 	@OneToMany(mappedBy = "categorieProduit")
