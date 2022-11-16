2cf67fec3e49a221ff91edd708e4c04da7c2863f
ImenChakroun <47691151+ImenChakroun@users.noreply.github.com>
2022-11-16 11:23:52 +0100

    Update Produit.java

--- src/main/java/com/esprit/examen/entities/Produit.java
+++ src/main/java/com/esprit/examen/entities/Produit.java
@@ -26,7 +26,7 @@ public class Produit implements Serializable {
 
 	/**
 	 * 
-	 */
+	 */ 
 	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
